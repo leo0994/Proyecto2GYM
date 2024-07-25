9451ae8c28413ac8c709bc715b3ba9791bffd82b
Matias Calvo <matiascalvoecheverria@gmail.com>
2024-07-24 23:11:52 -0600

    Equipment/Machine functionality

--- API/Controllers/MachineController.cs
+++ API/Controllers/MachineController.cs
@@ -1,12 +1,14 @@
 using DTOs;
 using Managers;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [EnableCors("NocheCorsPolicy")]
     [ApiController]
+    [Route("api/[controller]/[action]")]
     public class MachineController : ControllerBase
     {
         private readonly MachineManager _manager;
@@ -17,36 +19,39 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(MachineDTO machine)
+        public IActionResult Create(MachineDTO machineDTO)
         {
-            _manager.Create(machine);
-            return Ok();
+            var result = _manager.Create(machineDTO);
+            return Ok(result);
         }
 
         [HttpGet]
         public IActionResult RetrieveAll()
         {
-            return Ok(_manager.RetrieveAll());
+            var result = _manager.RetrieveAll();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
         public IActionResult RetrieveById(int id)
         {
-            return Ok(_manager.RetrieveById(id));
+            var result = _manager.RetrieveById(id);
+            return Ok(result);
         }
 
         [HttpPut]
-        public IActionResult Update(MachineDTO machine)
+        public IActionResult Update(MachineDTO machineDTO)
         {
-            _manager.Update(machine);
-            return Ok();
+            var result = _manager.Update(machineDTO);
+            return Ok(result);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            _manager.Delete(id);
-            return Ok();
+            var machineDTO = new MachineDTO { Id = id };
+            var result = _manager.Delete(machineDTO);
+            return Ok(result);
         }
     }
 }
